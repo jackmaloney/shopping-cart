c2be9f8c63c0c45d9fdc800384e94d3115cc91b8
jackmaloney <jack.maloney@transformuk.com>
2017-03-22 14:36:06 +0000

    Add spec outline for offers service

---
+++ src/test/scala/service/OffersServiceSpec.scala
@@ -0,0 +1,42 @@
+package service
+
+import org.specs2.mutable.Specification
+
+class OffersServiceSpec extends Specification {
+
+  "buyOneGetOneFree" should {
+    "give one item for free when two of the same item" in {
+
+    }
+
+    "give one item for free when three of the same item" in {
+
+    }
+
+    "give two items for free when four of the same item" in {
+
+    }
+
+    "give no items for free when one of the same item" in {
+
+    }
+  }
+
+  "threeForThePriceOfTwo" should {
+    "give one item for free when three of the same item" in {
+
+    }
+
+    "give one item for free when four of the same item" in {
+
+    }
+
+    "give two items for free when six of the same item" in {
+
+    }
+
+    "give no items for free when one of the same item" in {
+
+    }
+  }
+}
